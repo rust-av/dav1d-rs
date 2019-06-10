2667834e887e15dade1dcfaf02646935f7d98bcb
Luni-4 <luni-4@hotmail.it>
2019-06-11 01:07:28 +0200

    Build dav1d internally

--- dav1d-sys/build.rs
+++ dav1d-sys/build.rs
@@ -6,19 +6,90 @@ extern crate pkg_config;
 
 use std::env;
 use std::fs::File;
+use std::io::Write;
 use std::path::PathBuf;
 
-fn main() {
-    let libs = pkg_config::Config::new().probe("dav1d").unwrap();
+#[cfg(feature = "build")]
+mod build {
 
-    use std::io::Write;
+    use super::*;
+    use std::path::Path;
+    use std::process::Command;
+
+    const REPO: &'static str = "https://code.videolan.org/videolan/dav1d.git";
+    const OUTPUT_DIR: &'static str = "target";
+
+    macro_rules! runner {
+        ($cmd:expr, $($arg:expr),*) => {
+            Command::new($cmd)
+                $(.arg($arg))*
+                .output()
+                .expect(concat!("{} failed", $cmd));
+        };
+    }
+
+    pub fn build_from_src() {
+
+        let build_dir = "build";
+        let release_dir = "release";
+
+        let source = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap())
+            .join(OUTPUT_DIR);
+        let build_path = source.join(build_dir);
+        let release_path = source.join(release_dir);
+
+        if !Path::new(&source.join(".git")).exists() {
+            runner!("git", "clone", "--depth", "1", REPO, &source);
+        } else {
+            runner!("git", "-C", source.to_str().unwrap(), "pull");
+        }
+
+        runner!(
+            "meson",
+            "setup",
+            "-Ddefault_library=static",
+            "--prefix",
+            release_path.to_str().unwrap(),
+            build_path.to_str().unwrap(),
+            source.to_str().unwrap()
+        );
+        runner!("ninja", "-C", build_path.to_str().unwrap());
+        runner!("meson", "install", "-C", build_path.to_str().unwrap());
+
+        // Set pkg-config path
+        let key = "PKG_CONFIG_PATH";
+        let pkg_dir = "meson-private";
+        let value = format!("{}/{}", build_path.to_str().unwrap(), &pkg_dir);
+        env::set_var(key, &value);
+    }
+
+}
+
+#[cfg(feature = "build")]
+pub use self::build::build_from_src;
+
+#[cfg(not(feature = "build"))]
+fn build_from_src() {}
+
+fn main() {
+    if cfg!(feature = "build") {
+        build_from_src();
+    }
+
+    let libs = pkg_config::Config::new().probe("dav1d").unwrap();
 
     let headers = libs.include_paths.clone();
 
     let mut builder = bindgen::builder()
         .blacklist_type("max_align_t")
-        .rustfmt_bindings(false)
-        .header("data/dav1d.h");
+        .rustfmt_bindings(false);
+
+    if cfg!(feature = "build") {
+        let header_path = headers[0].join("dav1d/dav1d.h");
+        builder = builder.header(header_path.to_str().unwrap());
+    } else {
+        builder = builder.header("data/dav1d.h");
+    }
 
     for header in headers {
         builder = builder.clang_arg("-I").clang_arg(header.to_str().unwrap());
