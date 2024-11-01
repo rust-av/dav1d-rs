72a1e2a79c3b80f91b68234ca35c1bd7f1fc962e
haixuanTao <tao.xavier@outlook.com>
2024-11-01 04:20:39 +0100

    Bump dav1d version to 1.4

--- dav1d-sys/build.rs
+++ dav1d-sys/build.rs
@@ -7,7 +7,7 @@ mod build {
     use std::process::{Command, Stdio};
 
     const REPO: &str = "https://code.videolan.org/videolan/dav1d.git";
-    const TAG: &str = "1.3.0";
+    const TAG: &str = "1.4.0";
 
     macro_rules! runner {
         ($cmd:expr, $($arg:expr),*) => {
